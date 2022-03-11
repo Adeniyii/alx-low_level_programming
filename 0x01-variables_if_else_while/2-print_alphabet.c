1905236537fc96ea51aebad673c7de05f2224ec0
Adeniyii <ifedayoadeniyi@gmail.com>
2022-03-11 16:02:21 +0100

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 122, i;
+
+	for (i = 97; i < n; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
