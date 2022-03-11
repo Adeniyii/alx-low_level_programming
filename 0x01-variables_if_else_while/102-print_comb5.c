5f0c0b3e3593397a1eeb98b88f36a2d90e8adabe
Adeniyii <ifedayoadeniyi@gmail.com>
2022-03-11 17:23:20 +0100

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 57, i, j, k, l;
+
+	for (i = 48; i <= n; i++)
+	{
+		for (j = 48; j <= n; j++)
+		{
+			for (k = 48; k <= n; k++)
+			{
+				for (l = 48; l <= n; l++)
+				{
+					if (j != l)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(32);
+						putchar(k);
+						putchar(l);
+						if (i != 57 || j != 56 || k != 57 || l != 57)
+						{
+							putchar(44);
+							putchar(32);
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
