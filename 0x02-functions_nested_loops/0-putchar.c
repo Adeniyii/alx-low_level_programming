bd6362f249aa46d8cf8e4c663e7a45c927e0a30d
Adeniyii <ifedayoadeniyi@gmail.com>
2022-03-15 13:51:40 +0100

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,12 +10,14 @@ int main(void)
 {
 	char source[] = "_putchar";
 	int i, n = strlen(source);
-	
+
 	for (i = 0; i < n; i++)
 	{
 		int asciiVal = source[i];
+
 		putchar(asciiVal);
 	}
+
 	putchar(10);
 	return (0);
 }
