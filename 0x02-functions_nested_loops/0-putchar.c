9291a96c7cea501cc30e9933c3450909bb9533f2
Adeniyii <ifedayoadeniyi@gmail.com>
2022-03-15 13:47:10 +0100

    Write a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: Always success (0)
+  */
+int main(void)
+{
+	char source[] = "_putchar";
+	int i, n = strlen(source);
+	
+	for (i = 0; i < n; i++)
+	{
+		int asciiVal = source[i];
+		putchar(asciiVal);
+	}
+	putchar(10);
+	return (0);
+}
