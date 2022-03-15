3f92dee3e5b6adcb42680887b8e95a86bb4bcaf4
Adeniyii <ifedayoadeniyi@gmail.com>
2022-03-15 17:21:08 +0100

    fix-2

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar(43);
+		putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar(48);
+		putchar('0');
 		return (0);
 	}
 	else
 	{
-		putchar(45);
+		putchar('-');
 		return (-1);
 	}
 }
