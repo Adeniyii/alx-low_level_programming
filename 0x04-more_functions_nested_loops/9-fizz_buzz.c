a021a6cebc31bad1b88e79b8f6bbd181eb19ed7e
Adeniyii <ifedayoadeniyi@gmail.com>
2022-03-18 15:00:24 +0100

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - print squares to the terminal.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i != 1 && i != 100))
+			putchar(' ');
+
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
+	putchar(10);
+	return (0);
+}
