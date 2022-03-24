3f4556980cf425d6bc1b875fc9cb7aa390ca5055
Adeniyii <ifedayoadeniyi@gmail.com>
2022-03-24 05:07:22 +0100

    fix strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,15 +11,26 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+	int nulled = 0;
 
 	for (i = 0; i < n; i++)
 	{
 		if (*(src + i) == '\0')
 		{
 			*(dest + i) = '\0';
-			break;
+			nulled = 1;
 		}
-		*(dest + i) = *(src + i);
+		else
+		{
+			if (nulled)
+			{
+				*(dest + i) = '\0';
+			}
+			else
+			{
+				*(dest + i) = *(src + i);
+			}
+		}
 	}
 	return (dest);
 }
