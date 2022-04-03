c1459d31ff06111bdc7fbaf7c9f43e1e3c117e97
Adeniyii <ifedayoadeniyi@gmail.com>
2022-04-03 12:48:59 +0100

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: count of cmd line arguments.
+ * @argv: array of cmd line arguments.
+ *
+ * Return: Always (0).
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
+	return (0);
+}
