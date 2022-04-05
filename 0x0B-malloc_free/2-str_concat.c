627636b8e0b40cfcdf62abd47764f54da844082d
Adeniyii <ifedayoadeniyi@gmail.com>
2022-04-05 14:39:06 +0100

    fix concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,27 +14,31 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 	unsigned int i, j;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		return (NULL);
-	}
+		ptr = malloc(strlen(s2));
 
+		for (j = 0; j < strlen(s2); j++)
+			ptr[j] = s2[j];
+		return (ptr);
+	}
+	if (s2 == NULL)
+	{
+		ptr = malloc(strlen(s1));
+
+		for (j = 0; j < strlen(s1); j++)
+			ptr[j] = s1[j];
+		return (ptr);
+	}
 	ptr = (char *)malloc(strlen(s1) + strlen(s2) + 1);
 
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
 
 	for (i = 0; i < strlen(s1); i++)
-	{
 		ptr[i] = s1[i];
-	}
-
 	for (j = 0; j < strlen(s2); j++, i++)
-	{
 		ptr[i] = s2[j];
-	}
 
 	ptr[i] = '\0';
 	return (ptr);
