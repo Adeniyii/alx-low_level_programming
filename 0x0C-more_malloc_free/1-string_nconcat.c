5574ac6dab864c904f80d93f9e6dc00ecfaa09a9
Adeniyii <ifedayoadeniyi@gmail.com>
2022-04-07 19:57:07 +0100

    Write a function that concatenates two strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/* Function Declarations */
+unsigned int min(unsigned int a, unsigned int b);
+
+/**
+ * string_nconcat - concat two strings
+ *
+ * @s1: first string.
+ * @s2: second string.
+ * Return: (char*) address of concatenated string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, target_string_length = 0;
+	char *ptr;
+
+	if (s1 != NULL)
+		target_string_length += strlen(s1);
+
+	if (s2 != NULL)
+		target_string_length += min(strlen(s2), n);
+
+	ptr = malloc(target_string_length + 1);
+
+	if (s1 != NULL)
+	{
+		for (i = 0; i < strlen(s1); i++)
+		{
+			ptr[i] = s1[i];
+		}
+	}
+
+	if (s2 != NULL)
+	{
+		for (j = 0; j < min(strlen(s2), n); j++, i++)
+		{
+			ptr[i] = s2[j];
+		}
+	}
+	ptr[i] = '\0';
+
+	return (ptr);
+}
+
+/**
+ * min - return minimum of two numbers
+ *
+ * @a: first number to compare.
+ * @b: second number to compare.
+ * Return: min of two numbers
+ */
+unsigned int min(unsigned int a, unsigned int b)
+{
+	return (a > b) ? a : b;
+}
