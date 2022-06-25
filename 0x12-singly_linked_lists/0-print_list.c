9386ea4f60f707069d1fb321c786f1bfd9935e9f
Adeniyii <ifedayoadeniyi@gmail.com>
2022-06-25 17:49:41 +0100

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,11 +9,11 @@
 size_t print_list(const list_t *h)
 {
 	const list_t *cur = h;
-	int nodeCount = 0;
+	size_t nodeCount = 0;
 
 	while (cur)
 	{
-		printf("[%d] %s \n", cur->str ? cur->len : 0, cur->str ? cur->str : "(nil)");
+		printf("[%u] %s \n", cur->str ? cur->len : 0, cur->str ? cur->str : "(nil)");
 		cur = cur->next;
 		nodeCount++;
 	}
