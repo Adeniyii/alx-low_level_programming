5ccba7e683bf8247206ed2c2a1b45fd18fd9a720
Adeniyii <ifedayoadeniyi@gmail.com>
2022-06-25 17:45:05 +0100

    doc: fix formatting

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * print_list - A function that prints all the elements of a list_t list
+ * @h: the linked list
+ *
+ * Return: the number of nodes
+ */
 size_t print_list(const list_t *h)
 {
 	const list_t *cur = h;
