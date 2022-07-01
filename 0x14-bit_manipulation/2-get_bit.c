53c8ff5ee3d594dbc5d0b48f50f3b350f074ac01
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-02 00:10:05 +0100

    get bit value at index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_bit - Get the bit value at index
+ * @n: input number
+ * @index: position of bit to get
+ *
+ * Return: value of bit at given index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int mask = 1 << index;
+
+	if (mask & n)
+		return (1);
+
+	return (0);
+}
