ebfe06ba2a27de38197b9fe7bc9ee9bbe037f0f4
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-02 23:04:51 +0100

    handle all edge cases

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,12 +13,12 @@
 int create_file(const char *filename, char *text_content)
 {
 	ssize_t w_bytes;
-	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0700);
+	int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 
 	if (!filename || fd < 0)
 		return (-1);
 
-	if (text_content)
+	if (text_content != NULL)
 	{
 		w_bytes = write(fd, text_content, strlen(text_content));
 
