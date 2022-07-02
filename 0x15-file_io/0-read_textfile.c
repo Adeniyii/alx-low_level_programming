9517399f0bab3b068de217310fc923d9cd873bac
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-02 21:37:46 +0100

    read file into memory first

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,21 +11,25 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	char *buf;
 	size_t r_bytes;
-	int fd = open(filename, O_RDONLY, 0600);
+	int fd = open(filename, O_RDWR, 0600);
 
 	if (!filename || fd < 0)
 	{
 		return (0);
 	}
 
-	r_bytes = write(STDOUT_FILENO, filename, letters);
+	buf = (char *)malloc(letters);
+	read(fd, buf, letters);
+	r_bytes = write(STDOUT_FILENO, buf, letters);
 
 	if (r_bytes != letters)
 	{
 		return (0);
 	}
 
+	free(buf);
 	close(fd);
 	return (r_bytes);
 }
