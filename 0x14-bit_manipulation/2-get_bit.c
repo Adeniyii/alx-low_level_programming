c160b8b9d39f47f49d6251f0f4517710aa8dbad1
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-02 10:42:00 +0100

    fix

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,7 +14,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	if (!n)
 		return (-1);
 
-	if (mask & n)
+	if ((mask & n) > 0)
 		return (1);
 
 	return (0);
--- 0x14-bit_manipulation/2-main.c
+++ 0x14-bit_manipulation/2-main.c
@@ -16,5 +16,9 @@ int main(void)
 	printf("%d\n", n);
 	n = get_bit(1024, 0);
 	printf("%d\n", n);
+	n = get_bit(2, 0);
+	printf("%d\n", n);
+	n = get_bit(7, 0);
+	printf("%d\n", n);
 	return (0);
 }
