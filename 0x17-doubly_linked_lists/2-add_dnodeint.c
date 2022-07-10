6e3ebddc6959abefc72fcf3b1afda6f50441caec
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-10 21:24:07 +0100

    insert node at linked list head

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,6 +14,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 
+	if (!new_node)
+	{
+		return (NULL);
+	}
+
 	new_node->n = n;
 	new_node->next = NULL;
 	new_node->prev = NULL;
