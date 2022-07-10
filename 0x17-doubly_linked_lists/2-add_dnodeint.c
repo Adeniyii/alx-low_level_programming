3a97305d2fcb84325fdb592a8ceae8630893ad29
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-10 21:22:23 +0100

    insert node at linked list head

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+ * add_dnodeint - insert node at head of linked list
+ *
+ * @head: pointer to pointer to linked list
+ * @n: data for new node
+ * Return: address of newly inserted node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
