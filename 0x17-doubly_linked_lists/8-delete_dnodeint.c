9a0a674d15d8d361801c5247e024a9c29fc848d5
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-11 16:00:44 +0100

    fix

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -16,7 +16,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *tmp;
 	unsigned int i = 0;
 
-	if (!head || index >= dlistint_len(*head))
+	if (!head || !(*head) || index >= dlistint_len(*head))
 		return (-1);
 
 	while (ptr)
