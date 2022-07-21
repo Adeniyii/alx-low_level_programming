f0089842031e9277fe3735acfc1519d1a8a9d832
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-21 15:54:59 +0100

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,17 +11,21 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	size_t index;
 	char *value_cpy;
-	size_t index = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *new_hash_node = (hash_node_t *)malloc(sizeof(hash_node_t));
+	hash_node_t *new_hash_node;
 
-	if (!key)
+	if (!key || strcmp(key, "") == 0)
 		return (0);
+
+	new_hash_node = (hash_node_t *)malloc(sizeof(hash_node_t));
 
 	if (!new_hash_node)
 		return (0);
 
+	index = key_index((const unsigned char *)key, ht->size);
 	value_cpy = malloc((sizeof(char) * strlen(value)) + 1);
+	strcpy(value_cpy, value);
 
 	if (!value_cpy)
 	{
@@ -30,10 +34,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	new_hash_node->key = (char *)key;
+	new_hash_node->value = value_cpy;
 	new_hash_node->next = NULL;
-	new_hash_node->value = strcpy(value_cpy, value);
 
-	if (!ht->array[index])
+	if (!(ht->array[index]))
 	{
 		ht->array[index] = new_hash_node;
 		return (1);
