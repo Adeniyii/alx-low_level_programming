ca3ff0215f56a07c75730947b882705372a2e634
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-21 18:24:14 +0100

    check for duplicate key by bucket

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <string.h>
 
+int set_duplicate_key(const char *key, const char *value, hash_node_t *tmp);
+
 /**
  * hash_table_set - adds an element to the hash table
  * @ht: hash table to update
@@ -18,8 +20,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!key || *key == '\0' || !ht || !value)
 		return (0);
 
-	new_hash_node = (hash_node_t *)malloc(sizeof(hash_node_t));
+	index = key_index((const unsigned char *)key, ht->size);
+	if (set_duplicate_key(key, value, ht->array[index]))
+		return (1);
 
+	new_hash_node = (hash_node_t *)malloc(sizeof(hash_node_t));
 	if (!new_hash_node)
 		return (0);
 
@@ -33,8 +38,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 
-	index = key_index((const unsigned char *)key, ht->size);
-
 	if (!(ht->array[index]))
 	{
 		ht->array[index] = new_hash_node;
@@ -44,4 +47,25 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_hash_node->next = ht->array[index];
 	ht->array[index] = new_hash_node;
 	return (1);
+}
+
+/**
+ * set_duplicate_key - Set new value if key already exists in table
+ * @tmp: pointer to linked list head of current bucket
+ * @key: the key to be used for a hash table entry
+ * @value: value associated with the key @key
+ * Return: int (1) if successful, (0) otherwise
+ */
+int set_duplicate_key(const char *key, const char *value, hash_node_t *tmp)
+{
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			tmp->value = (char *)value;
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	return (0);
 }
