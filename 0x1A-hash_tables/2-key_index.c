426efd3986d0680ea4969f65d8f874bdc826ffb3
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-21 14:25:56 +0100

    index a key

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * key_index - get the index of a key
+ * @key: key to be indexed
+ * @size: size of the array of the hash table
+ * Return: (unsigned long int) the index of the key
+ */
+size_t key_index(const unsigned char *key, __attribute((unused)) size_t size)
+{
+	return (hash_djb2(key));
+}
