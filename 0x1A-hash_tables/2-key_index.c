62302394c4b4b797d7277840387b6534430b9abc
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-21 14:40:29 +0100

    fix hash result

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -7,7 +7,7 @@
  * @size: size of the array of the hash table
  * Return: (unsigned long int) the index of the key
  */
-size_t key_index(const unsigned char *key, __attribute((unused)) size_t size)
+size_t key_index(const unsigned char *key, size_t size)
 {
 	return (hash_djb2(key) % size);
 }
