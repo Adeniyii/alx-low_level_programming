1c6f3c0e794da003ef2c90edbd7ad2c1739f382f
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-21 18:29:26 +0100

    free dup string

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,6 +34,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (new_hash_node->value == NULL)
 	{
+		free(new_hash_node->value);
 		free(new_hash_node);
 		return (0);
 	}
