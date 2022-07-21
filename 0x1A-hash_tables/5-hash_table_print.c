e389d2822a949aad123dbf9681059aab84ec2ed2
Adeniyii <ifedayoadeniyi@gmail.com>
2022-07-21 21:06:47 +0100

    print hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *tmp;
+	int first_loop = 1;
+
+	if (!ht)
+	{
+		printf("{}");
+		return;
+	}
+
+	printf("{");
+
+	for (i = 0, tmp = ht->array[0]; i < ht->size; i++, tmp = ht->array[i])
+	{
+		while (tmp)
+		{
+			if (!first_loop)
+				printf(", ");
+
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			tmp = tmp->next;
+
+			if (tmp)
+				printf(", ");
+
+			first_loop = 0;
+		}
+	}
+	printf("}\n");
+}
