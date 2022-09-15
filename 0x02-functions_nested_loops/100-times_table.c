d986192f44fc81f3bc30b9040ded24840b39f589
Adeniyii <ifedayoadeniyi@gmail.com>
2022-09-15 20:30:21 +0100

    more mandatory tasks

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,29 +1,50 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * print_times_table - prints the n times table, starting with 0
- * @n Factor of multiplication table
+ * @n: number of the times table
  */
 void print_times_table(int n)
 {
-	int rone, cone, d;
+	int i, j, k;
 
-	for (rone = 0; rone <= n; rone++)
+	if (n >= 0 && n <= 15)
 	{
-		printf("0, ");
-		for (cone = 1; cone <= n; cone++)
+		for (i = 0; i <= n; i++)
 		{
-			d = (rone * cone);
-			if (d < 10)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(' ');
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				}
+				else if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				else if (k >= 10 && k < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
 			}
-			printf("%d", d);
-			if (cone < n)
-			{
-				printf(", ");
-			}
+			_putchar('\n');
 		}
-		printf("\n");
 	}
 }
