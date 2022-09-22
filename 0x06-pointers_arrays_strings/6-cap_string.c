e8e21f45dc9f033d114775a29d05de430716e96c
Adeniyii <ifedayoadeniyi@gmail.com>
2022-09-22 17:00:33 +0100

    implement complex cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 
+/* Helper functions */
+int check_is_lower(char c);
+int check_is_separator(char c);
+
 /**
  * cap_string - capitalize sentence
  * @text: chars to convert.
@@ -9,29 +13,47 @@
  */
 char *cap_string(char *text)
 {
-	int i;
+	char *tmp = text;
+	int capitalize_next = 1;
+	int is_lower, is_separator;
 
-	for (i = 0; text[i] != '\0'; i++)
+	while (*text)
 	{
-		if (text[i] >= 97 && text[i] <= 122)
-		{
-			if (i == 0)
-			{
-				text[i] = text[i] - 32;
-				continue;
-			}
+		is_lower = check_is_lower(*text);
+		is_separator = !is_lower && check_is_separator(*text);
 
-			if ((text[i - 1] >= 32 && text[i - 1] <= 47) ||
-			    (text[i - 1] >= 58 && text[i - 1] <= 63) ||
-			    (text[i - 1] >= 7 && text[i - 1] <= 13))
-			{
-				if (text[i - 1] == 45 ||
-				    (text[i - 1] >= 23 && text[i - 1] <= 27))
-					continue;
+		if (is_lower && capitalize_next)
+			*text = *text - 32; /* convert char to uppercase*/
 
-				text[i] = text[i] - 32;
-			}
-		}
+		if (is_separator)
+			capitalize_next = 1;
+		else
+			capitalize_next = 0;
+
+		text++;
 	}
-	return (text);
+	return (tmp);
+}
+
+/**
+ * check_is_lower - check if char is a lower-case character.
+ */
+int check_is_lower(char c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+
+	return (0);
+}
+
+/**
+ * check_is_separator - check if char is a specified separator.
+ */
+int check_is_separator(char c)
+{
+	if (c == 10 || c == 9 || c == 32 || c == 33 || c == 34 || c == 40 ||
+	    c == 41 || c == 44 || c == 46 || c == 59 || c == 63 || c == 123 || c == 125)
+		return (1);
+
+	return (0);
 }
