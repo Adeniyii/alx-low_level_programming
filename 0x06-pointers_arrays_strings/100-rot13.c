a3011072f7db087fe4be15a73602b67e3d6b247b
Adeniyii <ifedayoadeniyi@gmail.com>
2022-09-22 19:23:17 +0100

    implement rot-13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,63 +1,30 @@
 #include "main.h"
 #include <stdio.h>
 
-/* Helper Function Declaration*/
-char encodeChar(char min, char max, char currChar);
-
 /**
- * rot13 - encode a string using rot13
- * @text: chars to encode.
+ * rot13 - encoder rot13
+ * @s: pointer to string params
  *
- * Return: address to encoded chars.
+ * Return: *s
  */
-char *rot13(char *text)
+
+char *rot13(char *s)
 {
 	int i;
-	const int MIN_UPPER = 65, MAX_UPPER = 90;
-	const int MIN_LOWER = 97, MAX_LOWER = 122;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; text[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if ((text[i] >= MIN_UPPER && text[i] <= MAX_UPPER))
+		for (j = 0; j < 52; j++)
 		{
-			text[i] = encodeChar(MIN_UPPER, MAX_UPPER, text[i]);
-		}
-		else if ((text[i] >= MIN_LOWER && text[i] <= MAX_LOWER))
-		{
-			text[i] = encodeChar(MIN_LOWER, MAX_LOWER, text[i]);
-		}
-		else
-		{
-			continue;
+			if (s[i] == data1[j])
+			{
+				s[i] = datarot[j];
+				break;
+			}
 		}
 	}
-	return (text);
-}
-
-/**
- * encodeChar - move char by KEY places
- * @min: lower limit of ascii alphabets.
- * @max: upper limit of ascii alphabets.
- * @currChar: char to move.
- *
- * Return: encoded char.
- */
-char encodeChar(char min, char max, char currChar)
-{
-	const int KEY = 13, APLHA_COUNT = 26;
-	int places, position, offset, encoded;
-
-	places = KEY % APLHA_COUNT;
-	position = places + currChar;
-
-	if (position > max)
-	{
-		offset = (position - max) - 1;
-		encoded = min + offset;
-	}
-	else
-	{
-		encoded = position;
-	}
-	return (encoded);
+	return (s);
 }
